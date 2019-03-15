2beb58d859b8b2d4b8efd0d7c8caf397505685de
userir <userir@strech.esisar.fr>
2019-03-15 16:01:10 +0100

    Ajout

---
+++ api.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "api.h"
+
+
+typedef struct Arbre{
+  struct Arbre **filsTab; //Tableau de pointeurs vers les fils
+  struct Arbre *pere;  //Pointeur vers le pere
+  int nbFils; //Pour connaitre le nombre de fils du tableau de pointeurs
+  char* nomChamp;
+  char* valeurChamp;
+} noeud;
+
+
+// Fonction qui retourne un pointeur (type opaque) vers la racine de l'arbre construit.
+void *getRootTree();
+// Fonction qui recherche dans l'arbre tous les noeuds dont l'etiquette est egale à la chaine de caractères en argument.
+// Par convention si start == NULL alors on commence à la racine
+// sinon on effectue une recherche dans le sous-arbre à partir du noeud start
+_Token *searchTree(void *start,char *name);
+// fonction qui renvoie un pointeur vers char indiquant l'etiquette du noeud. (le nom de la rulename, intermediaire ou terminal)
+// et indique (si len!=NULL) dans *len la longueur de cette chaine.
+char *getElementTag(void *node,int *len);
+// fonction qui renvoie un pointeur vers char indiquant la valeur du noeud. (la partie correspondnant à la rulename dans la requete HTTP )
+// et indique (si len!=NULL) dans *len la longueur de cette chaine.
+char *getElementValue(void *node,int *len);
+// Fonction qui supprime et libere la liste chainée de reponse.
+void purgeElement(_Token **r);
+// Fonction qui supprime et libere toute la mémoire associée à l'arbre .
+void purgeTree(void *root){
+  //Penser a parcourir tout l'arbre et a free chaque fils de chaque branche
+}
+
+
+
+
+// L'appel à votre parser un char* et une longueur à parser.
+int parseur(char *req, int len){
+  return 0;
+}
